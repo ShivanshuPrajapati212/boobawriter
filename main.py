8cd745ae395446fdb7cfdf8c155b9a184e4dad5c
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-03-27 15:45:25 +0530

    yay some hope, i got it working on paper multi line but a bit scuffed and don't forget the color difference

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from PIL import Image
 
+
+
+width = 16 
+height = 33 
+x_offset = 193
+y_offset = 118
+line_gap = 10
+
 def getText():
     text = [] 
     with open("text.txt", "r") as f:
@@ -11,12 +19,10 @@ def getText():
 
 
 def createImage(text: list[str]):
-    width = 16 
-    height = 33 
-    total_width = width * (max(len(s) for s in text))
-    total_height = height * len(text) 
+    # total_width = width * (max(len(s) for s in text))
+    # total_height = height * len(text) 
 
-    image = Image.new("RGB", (total_width, total_height))
+    image = Image.open("paper.jpeg")
 
     for line_no, line in enumerate(text):
         for idx, letter in enumerate(line):
@@ -25,11 +31,11 @@ def createImage(text: list[str]):
             if letter.islower() == True:
                 letter = letter + "-Lower"
 
-            img = Image.open(f"bw_images/{letter}.jpeg")
+            img = Image.open(f"shivanshu-handwriting/{letter}.jpeg")
 
             scaled = img.resize((width, height))
 
-            image.paste(scaled, (width * idx, height * line_no))
+            image.paste(scaled, (width * idx + x_offset, height * line_no + y_offset + line_gap*line_no))
     
     image.save("output.jpg")
 
